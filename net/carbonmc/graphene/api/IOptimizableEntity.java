9357b4d37757d58f44aa84f18f1b7e9420905eb2
ZCRAFT <1149461164@qq.com>
2025-10-08 22:07:13 +0800

    -

--- net/carbonmc/graphene/api/IOptimizableEntity.java
+++
@@ -1,8 +0,0 @@
-package net.carbonmc.graphene.api;
-
-public interface IOptimizableEntity {
-    boolean shouldAlwaysTick();
-    void setAlwaysTick(boolean value);
-    boolean shouldTickInRaid();
-    void setTickInRaid(boolean value);
-}
