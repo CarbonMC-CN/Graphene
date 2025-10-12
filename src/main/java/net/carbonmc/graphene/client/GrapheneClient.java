fd154d901a7ca1a7ecfbecfdd5e0e3964a8dd07d
ZCRAFT <1149461164@qq.com>
2025-10-12 17:49:16 +0800

    语言文件

--- src/main/java/net/carbonmc/graphene/client/GrapheneClient.java
+++ src/main/java/net/carbonmc/graphene/client/GrapheneClient.java
@@ -1,40 +1,25 @@
 package net.carbonmc.graphene.client;
 
 import net.carbonmc.graphene.client.gui.ClothConfigScreenFactory;
-import net.carbonmc.graphene.client.gui.GUIEN;
 import net.carbonmc.graphene.engine.cull.AsyncTracer;
 import net.carbonmc.graphene.util.Fpsu;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
 
 @OnlyIn(Dist.CLIENT)
 public class GrapheneClient {
     public static Fpsu displayController;
     public static void init() {
         MinecraftForge.EVENT_BUS.register(ItemCountRenderer.class);
-        String language = Minecraft.getInstance().options.languageCode;
-        if ("zh_cn".equals(language)) {
             ModLoadingContext.get().registerExtensionPoint(
                     ConfigScreenHandler.ConfigScreenFactory.class,
                     () -> new ConfigScreenHandler.ConfigScreenFactory(
                             (mc, parent) -> ClothConfigScreenFactory.create(parent)
                     )
             );
-        }
-        else {
-            ModLoadingContext.get().registerExtensionPoint(
-                    ConfigScreenHandler.ConfigScreenFactory.class,
-                    () -> new ConfigScreenHandler.ConfigScreenFactory(
-                            (mc, parent) -> GUIEN.create(parent)
-                    )
-            );
-        }
     }
     public static void stop(){
         AsyncTracer.shutdown();
--- src/main/java/net/carbonmc/graphene/client/gui/ClothConfigScreenFactory.java
+++ src/main/java/net/carbonmc/graphene/client/gui/ClothConfigScreenFactory.java
@@ -19,105 +19,104 @@ public final class ClothConfigScreenFactory {
     public static Screen create(Screen parent) {
         ConfigBuilder builder = ConfigBuilder.create()
                 .setParentScreen(parent)
-                .setTitle(Component.literal("⚙ Graphene 配置"))
+                .setTitle(Component.translatable("graphene.gui.title"))
                 .transparentBackground()
                 .setSavingRunnable(CoolConfig.SPEC::save);
 
         ConfigEntryBuilder eb = builder.entryBuilder();
 
         ConfigCategory compat = builder.getOrCreateCategory(Component.translatable("graphene.gui.title.high"));
-        compat.addEntry(bool(eb, "修复珍珠泄漏", CoolConfig.FIX_PEARL_LEAK));
-        compat.addEntry(bool(eb, "修复抛射物插值", CoolConfig.FIX_PROJECTILE_LERP));
+        compat.addEntry(bool(eb, "graphene.gui.name.high.fpleak", CoolConfig.FIX_PEARL_LEAK));
+        compat.addEntry(bool(eb, "graphene.gui.name.high.fplerp", CoolConfig.FIX_PROJECTILE_LERP));
 
-        ConfigCategory render = builder.getOrCreateCategory(Component.literal("渲染优化"));
-        render.addEntry(bool(eb, "无发光实体时跳过轮廓", CoolConfig.skipOutlineWhenNoGlowing));
+        ConfigCategory render = builder.getOrCreateCategory(Component.translatable("graphene.gui.title.render"));
+        render.addEntry(bool(eb, "graphene.gui.name.render.skip", CoolConfig.skipOutlineWhenNoGlowing));
 
-        SubCategoryBuilder fps = eb.startSubCategory(Component.literal("Fps"));
-        fps.add(bool(eb, "降低渲染延迟", CoolConfig.fpsoo));
-        fps.add(intSlider(eb, "填写屏幕刷新率", 10, 360, CoolConfig.UDT));
+        SubCategoryBuilder fps = eb.startSubCategory(Component.translatable("graphene.gui.title.render.fps"));
+        fps.add(bool(eb, "graphene.gui.name.render.fps.fpsoo", CoolConfig.fpsoo));
+        fps.add(intSlider(eb, "graphene.gui.name.render.fps.udt", 10, 360, CoolConfig.UDT));
         render.addEntry(fps.build());
 
-        SubCategoryBuilder leaf = eb.startSubCategory(Component.literal("树叶优化"));
-        leaf.add(bool(eb, "高级树叶剔除", CoolConfig.useAdvancedLeafCulling));
-        leaf.add(intSlider(eb, "最小树叶连接", 1, 6, CoolConfig.minLeafConnections));
-        leaf.add(bool(eb, "优化红树林", CoolConfig.OPTIMIZE_MANGROVE));
+        SubCategoryBuilder leaf = eb.startSubCategory(Component.translatable("graphene.gui.title.render.leaf"));
+        leaf.add(bool(eb, "graphene.gui.name.render.leaf.ualc", CoolConfig.useAdvancedLeafCulling));
+        leaf.add(intSlider(eb, "graphene.gui.name.render.leaf.mlc", 1, 6, CoolConfig.minLeafConnections));
+        leaf.add(bool(eb, "graphene.gui.name.render.leaf.om", CoolConfig.OPTIMIZE_MANGROVE));
         render.addEntry(leaf.build());
-        SubCategoryBuilder trace = eb.startSubCategory(Component.literal("路径追踪"));
-        trace.add(intSlider(eb, "追踪线程数", 1, 8, CoolConfig.tracingThreads));
-        trace.add(doubleField(eb, "追踪距离", 1, 16, CoolConfig.traceDistance));
-        trace.add(doubleField(eb, "回退距离", 4, 32, CoolConfig.fallbackDistance));
+        SubCategoryBuilder trace = eb.startSubCategory(Component.translatable("graphene.gui.title.render.trace"));
+        trace.add(intSlider(eb, "graphene.gui.name.render.trace.tT", 1, 8, CoolConfig.tracingThreads));
+        trace.add(doubleField(eb, "graphene.gui.name.render.trace.tD", 1, 16, CoolConfig.traceDistance));
+        trace.add(doubleField(eb, "graphene.gui.name.render.trace.fbD", 4, 32, CoolConfig.fallbackDistance));
         render.addEntry(trace.build());
 
-        SubCategoryBuilder chest = eb.startSubCategory(Component.literal("箱子渲染优化"));
-        chest.add(bool(eb, "启用优化", CoolConfig.ENABLE_OPTIMIZATION));
-        chest.add(enumOpt(eb, "渲染模式", CoolConfig.RenderMode.class, CoolConfig.RENDER_MODE));
-        chest.add(bool(eb, "优化末影箱", CoolConfig.OPTIMIZE_ENDER_CHESTS));
-        chest.add(bool(eb, "优化陷阱箱", CoolConfig.OPTIMIZE_TRAPPED_CHESTS));
-        chest.add(intSlider(eb, "最大渲染距离", 1, 128, CoolConfig.MAX_RENDER_DISTANCE));
+        SubCategoryBuilder chest = eb.startSubCategory(Component.translatable("graphene.gui.title.render.chest"));
+        chest.add(bool(eb, "graphene.gui.name.render.chest.EO", CoolConfig.ENABLE_OPTIMIZATION));
+        chest.add(enumOpt(eb, "graphene.gui.name.render.chest.RM", CoolConfig.RenderMode.class, CoolConfig.RENDER_MODE));
+        chest.add(bool(eb, "graphene.gui.name.render.chest.OEC", CoolConfig.OPTIMIZE_ENDER_CHESTS));
+        chest.add(bool(eb, "graphene.gui.name.render.chest.OTC", CoolConfig.OPTIMIZE_TRAPPED_CHESTS));
+        chest.add(intSlider(eb, "graphene.gui.name.render.chest.MRD", 1, 128, CoolConfig.MAX_RENDER_DISTANCE));
         render.addEntry(chest.build());
-
-        ConfigCategory reflex = builder.getOrCreateCategory(Component.literal("Reflex 低延迟"));
-        reflex.addEntry(bool(eb, "启用 Reflex", CoolConfig.enableReflex));
-        reflex.addEntry(longField(eb, "Reflex 偏移 (ns)",
+        SubCategoryBuilder inactive = eb.startSubCategory(Component.translatable("graphene.gui.title.render.inactive"));
+        inactive.add(bool(eb, "graphene.gui.name.render.inactive.RFWI", CoolConfig.REDUCE_FPS_WHEN_INACTIVE));
+        inactive.add(intSlider(eb, "graphene.gui.name.render.inactive.IFL", 5, 60, CoolConfig.INACTIVE_FPS_LIMIT));
+        inactive.add(bool(eb, "graphene.gui.name.render.inactive.RRDWI", CoolConfig.REDUCE_RENDER_DISTANCE_WHEN_INACTIVE));
+        inactive.add(intSlider(eb, "graphene.gui.name.render.inactive.IRD", 2, 12, CoolConfig.INACTIVE_RENDER_DISTANCE));
+        render.addEntry(inactive.build());
+        ConfigCategory reflex = builder.getOrCreateCategory(Component.translatable("graphene.gui.title.reflex"));
+        reflex.addEntry(bool(eb, "graphene.gui.name.reflex.eR", CoolConfig.enableReflex));
+        reflex.addEntry(longField(eb, "graphene.gui.name.reflex.rO",
                 -1_000_000L, 1_000_000L, CoolConfig.reflexOffsetNs));
-        reflex.addEntry(intSlider(eb, "Reflex 帧率上限", 0, 1000, CoolConfig.MAX_FPS));
+        reflex.addEntry(intSlider(eb, "graphene.gui.name.reflex.MF", 0, 1000, CoolConfig.MAX_FPS));
 
 
-        ConfigCategory particle = builder.getOrCreateCategory(Component.literal("粒子优化"));
-        particle.addEntry(bool(eb, "启用粒子优化", CoolConfig.ENABLE_PARTICLE_OPTIMIZATION));
+        ConfigCategory particle = builder.getOrCreateCategory(Component.translatable("graphene.gui.title.particle"));
+        particle.addEntry(bool(eb, "graphene.gui.name.particle.EPO", CoolConfig.ENABLE_PARTICLE_OPTIMIZATION));
 
-        SubCategoryBuilder lod = eb.startSubCategory(Component.literal("LOD 系统"));
-        lod.add(bool(eb, "启用粒子LOD", CoolConfig.ENABLE_PARTICLE_LOD));
-        lod.add(doubleField(eb, "LOD距离阈值", 4, 64, CoolConfig.LOD_DISTANCE_THRESHOLD));
-        lod.add(doubleField(eb, "LOD减少因子", 0, 1, CoolConfig.LOD_REDUCTION_FACTOR));
+        SubCategoryBuilder lod = eb.startSubCategory(Component.translatable("graphene.gui.title.particle.lod"));
+        lod.add(bool(eb, "graphene.gui.name.particle.EPL", CoolConfig.ENABLE_PARTICLE_LOD));
+        lod.add(doubleField(eb, "graphene.gui.name.particle.LDH", 4, 64, CoolConfig.LOD_DISTANCE_THRESHOLD));
+        lod.add(doubleField(eb, "graphene.gui.name.particle.LRF", 0, 1, CoolConfig.LOD_REDUCTION_FACTOR));
         particle.addEntry(lod.build());
 
-        SubCategoryBuilder timestep = eb.startSubCategory(Component.literal("时间步长"));
-        timestep.add(bool(eb, "启用固定时间步长", CoolConfig.ENABLE_FIXED_TIMESTEP));
-        timestep.add(doubleField(eb, "时间步长间隔", 0.001, 0.1, CoolConfig.FIXED_TIMESTEP_INTERVAL));
+        SubCategoryBuilder timestep = eb.startSubCategory(Component.translatable("graphene.gui.title.particle.timestep"));
+        timestep.add(bool(eb, "graphene.gui.name.particle.EFT", CoolConfig.ENABLE_FIXED_TIMESTEP));
+        timestep.add(doubleField(eb, "graphene.gui.name.particle.FTI", 0.001, 0.1, CoolConfig.FIXED_TIMESTEP_INTERVAL));
         particle.addEntry(timestep.build());
 
-        SubCategoryBuilder lists = eb.startSubCategory(Component.literal("粒子列表"));
-        lists.add(stringList(eb, "LOD白名单", CoolConfig.LOD_PARTICLE_WHITELIST));
-        lists.add(stringList(eb, "LOD黑名单", CoolConfig.LOD_PARTICLE_BLACKLIST));
+        SubCategoryBuilder lists = eb.startSubCategory(Component.translatable("graphene.gui.title.particle.lists"));
+        lists.add(stringList(eb, "graphene.gui.name.particle.LPW", CoolConfig.LOD_PARTICLE_WHITELIST));
+        lists.add(stringList(eb, "graphene.gui.name.particle.LPB", CoolConfig.LOD_PARTICLE_BLACKLIST));
         particle.addEntry(lists.build());
 
-        SubCategoryBuilder inactive = eb.startSubCategory(Component.literal("非活动状态优化"));
-        inactive.add(bool(eb, "窗口失焦降帧率", CoolConfig.REDUCE_FPS_WHEN_INACTIVE));
-        inactive.add(intSlider(eb, "失焦FPS上限", 5, 60, CoolConfig.INACTIVE_FPS_LIMIT));
-        inactive.add(bool(eb, "失焦降视距", CoolConfig.REDUCE_RENDER_DISTANCE_WHEN_INACTIVE));
-        inactive.add(intSlider(eb, "失焦视距", 2, 12, CoolConfig.INACTIVE_RENDER_DISTANCE));
-        render.addEntry(inactive.build());
 
-        ConfigCategory light = builder.getOrCreateCategory(Component.literal("光照性能优化"));
-        light.addEntry(bool(eb, "光照优化", CoolConfig.ENABLE_FIXED_LIGHT));
-        light.addEntry(bool(eb, "竹子光照优化-取消竹子无意义的光照计算", CoolConfig.BambooLight));
+        ConfigCategory light = builder.getOrCreateCategory(Component.translatable("graphene.gui.title.light"));
+        light.addEntry(bool(eb, "graphene.gui.name.light.EFL", CoolConfig.ENABLE_FIXED_LIGHT));
+        light.addEntry(bool(eb, "graphene.gui.name.light.BL", CoolConfig.BambooLight));
 
 
-        ConfigCategory entity = builder.getOrCreateCategory(Component.literal("实体优化"));
-        entity.addEntry(bool(eb, "实体Tick优化", CoolConfig.optimizeEntities));
-        entity.addEntry(intSlider(eb, "水平范围", 1, 256, CoolConfig.horizontalRange));
-        entity.addEntry(intSlider(eb, "垂直范围", 1, 256, CoolConfig.verticalRange));
-        entity.addEntry(stringList(eb, "实体优化白名单", CoolConfig.entityWhitelist));
-        entity.addEntry(bool(eb, "忽略死亡实体", CoolConfig.ignoreDeadEntities));
-        entity.addEntry(bool(eb, "清理死亡实体", CoolConfig.OPTIMIZE_ENTITY_CLEANUP));
-        entity.addEntry(bool(eb, "袭击时保持袭击者Tick", CoolConfig.tickRaidersInRaid));
-        ConfigCategory item = builder.getOrCreateCategory(Component.literal("物品优化"));
-        item.addEntry(bool(eb, "启用物品优化", CoolConfig.OpenIO));
-        item.addEntry(intSlider(eb, "最大堆叠", -1, 9999, CoolConfig.maxStackSize));
-        item.addEntry(doubleField(eb, "合并半径", 0.1, 10, CoolConfig.mergeDistance));
-        item.addEntry(bool(eb, "掉落物锁", CoolConfig.lockMaxedStacks));
-        item.addEntry(intSlider(eb, "列表模式", 0, 2, CoolConfig.listMode));
-        item.addEntry(bool(eb, "显示堆叠数", CoolConfig.showStackCount));
-        item.addEntry(bool(eb, "启用自定义堆叠", CoolConfig.ENABLED));
-        item.addEntry(intSlider(eb, "自定义最大堆叠", 1, 9999, CoolConfig.MAX_STACK_SIZE));
-        item.addEntry(bool(eb, "物品实体Tick优化", CoolConfig.optimizeItems));
+        ConfigCategory entity = builder.getOrCreateCategory(Component.translatable("graphene.gui.title.entity"));
+        entity.addEntry(bool(eb, "graphene.gui.name.entity.oE", CoolConfig.optimizeEntities));
+        entity.addEntry(intSlider(eb, "graphene.gui.name.entity.hR", 1, 256, CoolConfig.horizontalRange));
+        entity.addEntry(intSlider(eb, "graphene.gui.name.entity.vR", 1, 256, CoolConfig.verticalRange));
+        entity.addEntry(stringList(eb, "graphene.gui.name.entity.eW", CoolConfig.entityWhitelist));
+        entity.addEntry(bool(eb, "graphene.gui.name.entity.iDE", CoolConfig.ignoreDeadEntities));
+        entity.addEntry(bool(eb, "graphene.gui.name.entity.OEC", CoolConfig.OPTIMIZE_ENTITY_CLEANUP));
+        entity.addEntry(bool(eb, "graphene.gui.name.entity.tRI", CoolConfig.tickRaidersInRaid));
+        ConfigCategory item = builder.getOrCreateCategory(Component.translatable("graphene.gui.title.item"));
+        item.addEntry(bool(eb, "graphene.gui.name.item.OIO", CoolConfig.OpenIO));
+        item.addEntry(intSlider(eb, "graphene.gui.name.item.mSS", -1, 9999, CoolConfig.maxStackSize));
+        item.addEntry(doubleField(eb, "graphene.gui.name.item.mD", 0.1, 10, CoolConfig.mergeDistance));
+        item.addEntry(bool(eb, "graphene.gui.name.item.lMS", CoolConfig.lockMaxedStacks));
+        item.addEntry(intSlider(eb, "graphene.gui.name.item.lM", 0, 2, CoolConfig.listMode));
+        item.addEntry(bool(eb, "graphene.gui.name.item.sSC", CoolConfig.showStackCount));
+        item.addEntry(bool(eb, "graphene.gui.name.item.ENABLED", CoolConfig.ENABLED));
+        item.addEntry(intSlider(eb, "graphene.gui.name.item.MSS", 1, 9999, CoolConfig.MAX_STACK_SIZE));
+        item.addEntry(bool(eb, "graphene.gui.name.item.oI", CoolConfig.optimizeItems));
 
-        ConfigCategory mem = builder.getOrCreateCategory(Component.literal("内存优化"));
-        mem.addEntry(bool(eb, "内存泄漏修复_AE2WTLibCreativeTabLeakFix", CoolConfig.MemoryLeakFix_AE2WTLibCreativeTabLeakFix));
-        mem.addEntry(bool(eb, "内存泄漏修复_ScreenshotByteBufferLeakFix", CoolConfig.MemoryLeakFix_ScreenshotByteBufferLeakFix));
-        ConfigCategory debug = builder.getOrCreateCategory(Component.literal("调试"));
-        debug.addEntry(bool(eb, "调试日志", CoolConfig.DEBUG_LOGGING));
+        ConfigCategory mem = builder.getOrCreateCategory(Component.translatable("graphene.gui.title.mem"));
+        mem.addEntry(bool(eb, "graphene.gui.name.mem.MLFAE", CoolConfig.MemoryLeakFix_AE2WTLibCreativeTabLeakFix));
+        mem.addEntry(bool(eb, "graphene.gui.name.mem.MLFSB", CoolConfig.MemoryLeakFix_ScreenshotByteBufferLeakFix));
+        ConfigCategory debug = builder.getOrCreateCategory(Component.translatable("graphene.gui.title.debug"));
+        debug.addEntry(bool(eb, "graphene.gui.name.debug.DL", CoolConfig.DEBUG_LOGGING));
 
         return builder.build();
     }
@@ -125,8 +124,8 @@ public final class ClothConfigScreenFactory {
     private static BooleanListEntry bool(ConfigEntryBuilder eb,
                                          String key,
                                          ForgeConfigSpec.BooleanValue value) {
-        return eb.startBooleanToggle(Component.literal(key), value.get())
-                .setTooltip(Component.literal(key))
+        return eb.startBooleanToggle(Component.translatable(key), value.get())
+                .setTooltip(Component.translatable(key))
                 .setSaveConsumer(value::set)
                 .build();
     }
@@ -137,9 +136,9 @@ public final class ClothConfigScreenFactory {
                                            long min,
                                            long max,
                                            ForgeConfigSpec.LongValue value) {
-        return eb.startLongField(Component.literal(key), value.get())
+        return eb.startLongField(Component.translatable(key), value.get())
                 .setMin(min).setMax(max)
-                .setTooltip(Component.literal(key))
+                .setTooltip(Component.translatable(key))
                 .setSaveConsumer(value::set)
                 .build();
     }
@@ -149,8 +148,8 @@ public final class ClothConfigScreenFactory {
                                                                 String key,
                                                                 Class<E> clazz,
                                                                 ForgeConfigSpec.EnumValue<E> value) {
-        return eb.startEnumSelector(Component.literal(key), clazz, value.get())
-                .setTooltip(Component.literal(key))
+        return eb.startEnumSelector(Component.translatable(key), clazz, value.get())
+                .setTooltip(Component.translatable(key))
                 .setSaveConsumer(value::set)
                 .build();
     }
@@ -158,8 +157,8 @@ public final class ClothConfigScreenFactory {
                                                   String key,
                                                   ForgeConfigSpec.ConfigValue<List<? extends String>> value) {
         List<String> currentValue = (List<String>) value.get();
-        return eb.startStrList(Component.literal(key), currentValue)
-                .setTooltip(Component.literal(key))
+        return eb.startStrList(Component.translatable(key), currentValue)
+                .setTooltip(Component.translatable(key))
                 .setSaveConsumer(value::set)
                 .build();
     }
@@ -168,8 +167,8 @@ public final class ClothConfigScreenFactory {
                                                 int min,
                                                 int max,
                                                 ForgeConfigSpec.IntValue value) {
-        return eb.startIntSlider(Component.literal(key), value.get(), min, max)
-                .setTooltip(Component.literal(key))
+        return eb.startIntSlider(Component.translatable(key), value.get(), min, max)
+                .setTooltip(Component.translatable(key))
                 .setSaveConsumer(value::set)
                 .build();
     }
@@ -179,9 +178,9 @@ public final class ClothConfigScreenFactory {
                                                double min,
                                                double max,
                                                ForgeConfigSpec.DoubleValue value) {
-        return eb.startDoubleField(Component.literal(key), value.get())
+        return eb.startDoubleField(Component.translatable(key), value.get())
                 .setMin(min).setMax(max)
-                .setTooltip(Component.literal(key))
+                .setTooltip(Component.translatable(key))
                 .setSaveConsumer(value::set)
                 .build();
     }
